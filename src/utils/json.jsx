9ec11ca642e10d929c4d590c2733313b87e0f97c
Xue Hongyan <1141563180@qq.com>
2019-11-11 21:21:10 +0800

    Add map edit

---
+++ src/utils/json.jsx
@@ -0,0 +1,19 @@
+import {jsonConfig} from '../config';
+
+class _JSON {
+  parse(text){
+    let jsonObj;
+    try {
+      jsonObj = JSON.parse(text);
+      return [jsonObj, null];
+    }
+    catch(e) {
+      return [null, e];
+    }
+  }
+  stringify(value){
+    return JSON.stringify(value, jsonConfig.replacer, jsonConfig.space);
+  }
+}
+
+export default new _JSON();
