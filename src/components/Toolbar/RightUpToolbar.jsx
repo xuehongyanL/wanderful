3e498fc9f3168b7b18d308a5039487c7ce06cac1
Xue Hongyan <1141563180@qq.com>
2019-11-23 15:36:15 +0800

    Add import/export button (step2)

--- src/components/Toolbar/RightUpToolbar.jsx
+++ src/components/Toolbar/RightUpToolbar.jsx
@@ -1,14 +1,45 @@
 import React from 'react';
-import {Button, ButtonGroup} from 'reactstrap';
+import {Button, ButtonGroup, Input} from 'reactstrap';
+
 import emitter from '../../ev';
 
 class RightUpToolbar extends React.Component {
-  toggle(){}
+  constructor(props){
+    super(props);
+    this.fileRef = React.createRef();
+    this.reader = new FileReader();
+    this.reader.onloadend = () => {
+      emitter.emit('text', this.reader.result);
+    };
+  }
+  _onImport(){
+    console.log(this.fileRef.current);
+    document.getElementById('fileInput').click();
+  }
+  _onExport(){
+    emitter.emit('save');
+  }
+  _onSelectFile(e){
+    this.reader.readAsText(e.target.files[0]);
+  }
   render(){
     return (
       <ButtonGroup size={'sm'}>
-        <Button color="secondary">Import</Button>
-        <Button color="secondary" onClick={()=>{emitter.emit('save');}}>Export</Button>
+        <Input
+          type="file"
+          id="fileInput"
+          ref={this.fileRef}
+          onChange={this._onSelectFile.bind(this)}
+          style={{display: 'none'}}
+        />
+        <Button
+          color="secondary"
+          onClick={this._onImport.bind(this)}
+        >Import</Button>
+        <Button
+          color="secondary"
+          onClick={this._onExport.bind(this)}
+        >Export</Button>
       </ButtonGroup>
     );
   }
